53f67e425b41c98cd8c139949a172d0d15cb9628
Tey Sui Zer <suizer@nus.edu.sg>
2023-10-13 20:49:23 +0800

    Moved conversion codes into subdir, created skeleton fastapi app

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+import uvicorn
+from typing import Union
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
