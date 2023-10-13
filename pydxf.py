a6b77117879695349f61e888aed68881d6313149
Tey Sui Zer <suizer@nus.edu.sg>
2023-10-13 19:28:25 +0800

    Preserve many fields as possible when converting to shp

--- pydxf.py
+++ pydxf.py
@@ -12,7 +12,8 @@ shp_file_path = os.path.splitext(dxf_file_path)[0] + ".shp"
 dxf_datasource = ogr.Open(dxf_file_path)
 print(dxf_datasource)
 
-def convert(dxfFile, shpFile, layerFilter='.*'):
+
+def convert(dxfFile, shpFile, layerFilter=".*"):
     # Check if the output shapefile already exists
     if path.exists(shpFile):
         # If it exists, delete it to replace it
@@ -22,29 +23,28 @@ def convert(dxfFile, shpFile, layerFilter='.*'):
     # Create the driver before the loop
     drv = ogr.GetDriverByName("ESRI Shapefile")
 
-    # open datasource
+    # Open the DXF data source
     ds = ogr.Open(dxfFile)
     if ds is None:
         return -1
 
     unique_geometry_types = set()  # To store unique geometry types
 
-    # Find all layers in the DXF file and iterate through them
+    # Iterate through features in the layer and collect unique geometry types and field names
+    field_names = set()  # To store unique field names
+
     for layer in ds:
-        layer_name = layer.GetName()  # Get the name of the layer
+        layer_name = layer.GetName()
         p = re.compile(layerFilter)
         if not p.search(layer_name) is None:
             lyr = ds.GetLayerByName(layer_name)
             lyr.ResetReading()
             feat_defn = lyr.GetLayerDefn()
             fid = {}
-            # find Layer & EntityHandle column in input
+
             for i in range(feat_defn.GetFieldCount()):
                 field_defn = feat_defn.GetFieldDefn(i)
-                if field_defn.GetName() == "Layer":
-                    fid["Layer"] = i
-                elif field_defn.GetName() == "EntityHandle":
-                    fid["EntityHandle"] = i
+                field_names.add(field_defn.GetName())
 
             # Iterate through features in the layer and collect unique geometry types
             for feat in lyr:
@@ -59,18 +59,12 @@ def convert(dxfFile, shpFile, layerFilter='.*'):
         do = drv.CreateDataSource(shp_layer_file)
         if do is None:
             return 1
-        lyro = do.CreateLayer("out", None, ogr.wkbUnknown)  # Use ogr.wkbUnknown to accept any geometry type
-        if lyro is None:
-            return 1
-        # add two fields to output LAYER and ENTITYHAND
-        field_defn = ogr.FieldDefn("LAYER", ogr.OFTString)
-        field_defn.SetWidth(32)
-        if lyro.CreateField(field_defn) != 0:
-            return 1
-        field_defn = ogr.FieldDefn("ENTITYHAND", ogr.OFTString)
-        field_defn.SetWidth(8)
-        if lyro.CreateField(field_defn) != 0:
-            return 1
+        lyro = do.CreateLayer("out", None, ogr.wkbUnknown)
+
+        # Create fields in the output shapefile for all field names found in the DXF file
+        for field_name in field_names:
+            field_defn = ogr.FieldDefn(field_name, ogr.OFTString)
+            lyro.CreateField(field_defn)
 
         # Find the layers containing the current geometry type and copy features to the corresponding shapefile
         for layer in ds:
@@ -79,15 +73,6 @@ def convert(dxfFile, shpFile, layerFilter='.*'):
             if not p.search(layer_name) is None:
                 lyr = ds.GetLayerByName(layer_name)
                 lyr.ResetReading()
-                feat_defn = lyr.GetLayerDefn()
-                fid = {}
-                for i in range(feat_defn.GetFieldCount()):
-                    field_defn = feat_defn.GetFieldDefn(i)
-                    if field_defn.GetName() == "Layer":
-                        fid["Layer"] = i
-                    elif field_defn.GetName() == "EntityHandle":
-                        fid["EntityHandle"] = i
-
                 for feat in lyr:
                     geom = feat.GetGeometryRef()
                     if geom is None:
@@ -95,13 +80,17 @@ def convert(dxfFile, shpFile, layerFilter='.*'):
                     if geom.GetGeometryName() == geom_type:
                         feato = ogr.Feature(lyro.GetLayerDefn())
                         feato.SetGeometry(geom)
-                        feato.SetField("LAYER", feat.GetField(fid["Layer"]))
-                        feato.SetField("ENTITYHAND", feat.GetField(fid["EntityHandle"]))
+
+                        # Copy all fields from the DXF feature to the output shapefile feature
+                        for field_name in field_names:
+                            feato.SetField(field_name, feat.GetField(field_name))
+
                         lyro.CreateFeature(feato)
 
     ds = None
     dso = None
     return 0
+
 
 # conversion
 def dxf2shp(dxf_file_path=None, shp_file_path=None):
@@ -110,13 +99,15 @@ def dxf2shp(dxf_file_path=None, shp_file_path=None):
     # enter geometry type ogr.wkbPoint | ogr.wkbLineString | ogr.wkbPolygon
     # shp_type = ogr.wkbLineString
     # enter regexp to filter layers
-    dxf_layer_filter = '.*'
+    dxf_layer_filter = ".*"
     res = convert(dxf_name, target_name, dxf_layer_filter)
     print(res)
     if res == 0:
         # load the shape
         (shpdir, shpfile) = path.split(target_name)
         print(f"{shpdir} {shpfile} is created")
+
+
 def matplot(dxf_datasource=None):
     # Iterate through layers and plot features
     for layer_idx in range(dxf_datasource.GetLayerCount()):
@@ -130,19 +121,20 @@ def matplot(dxf_datasource=None):
             x = []
             y = []
 
-            if geometry.GetGeometryName() == 'LINESTRING':
+            if geometry.GetGeometryName() == "LINESTRING":
                 for i in range(geometry.GetPointCount()):
                     x.append(geometry.GetX(i))
                     y.append(geometry.GetY(i))
 
                 # Plot the line
-                plt.plot(x, y, label=f'Layer {layer_idx + 1}')
+                plt.plot(x, y, label=f"Layer {layer_idx + 1}")
 
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('DXF File Visualization')
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("DXF File Visualization")
     plt.legend()
     plt.show()
+
 
 # matplot(dxf_datasource)
 dxf2shp(dxf_file_path, shp_file_path)
